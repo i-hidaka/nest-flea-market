b8247bae5378e650813e487a21fba83f79fa29f0
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-07-13 10:38:17 +0900

    itemsサービスを作成

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,11 @@
+import { ItemsService } from './items.service';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly ItemsService: ItemsService) {}
   @Get()
   findAll() {
-    return 'this is findAll';
+    return this.ItemsService.findAll();
   }
 }
